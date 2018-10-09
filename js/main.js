781c213ee247ccb3356a78039112b1845d36da3a
darincardin <darin.cardin@soterianetworks.com>
2018-10-09 17:10:14 -0400

    overhaul

--- js/main.js
+++ js/main.js
@@ -17,28 +17,30 @@ app.controller("HeaderCtrl", function($scope, $location) {
 
 
 app.controller("HomeCtrl", function($scope, $location) {
-     $scope.$parent.$parent.title ="Home"
-     $scope.$parent.$parent.subtitle ="What is Soteria Networks"
-     $scope.$parent.$parent.class ="home-bg"
+    $scope.$parent.$parent.title ="Home"
+    $scope.$parent.$parent.class ="home-bg"
 });
 
 app.controller("ProductsCtrl", function($scope, $location, $timeout) {
     $scope.$parent.$parent.title ="Products"
-    $scope.$parent.$parent.subtitle ="Take a closer look at our products."
     $scope.$parent.$parent.class ="products-bg"
-    $scope.showBg = false
-    $scope.show = {};
-
-    $scope.hoverIn = function(p)  { 
-     // console.log('aaa')
-    //  $scope.show[p] = $scope.showBg = true; 
-    }
-    $scope.hoverOut = function(p) { 
-     // console.log('bbb')
-      //$scope.show[p] = $scope.showBg = false;  
-    }
 });
 
+
+app.controller("Products-CIP-Ctrl", function($scope, $location) {
+    $scope.$parent.$parent.title ="Products: CIP"
+    $scope.$parent.$parent.class ="products-bg"
+});
+
+app.controller("Products-MES-Ctrl", function($scope, $location) {
+    $scope.$parent.$parent.title ="Products: MES"
+    $scope.$parent.$parent.class ="products-bg"
+});
+
+app.controller("Products-IPS-Ctrl", function($scope, $location) {
+    $scope.$parent.$parent.title ="Products: IPS"
+    $scope.$parent.$parent.class ="products-bg"
+});
 
 app.controller("SolutionsCtrl", function($scope, $location) {
      $scope.$parent.$parent.title ="Solutions"
@@ -61,12 +63,14 @@ app.controller("AboutUsCtrl", function($scope, $location) {
 app.config(function($routeProvider) {
     $routeProvider
     .when("/home",      {  templateUrl : "html/home.html"  })
+
     .when("/products",  {  templateUrl : "html/products.html"  })
     .when("/products/cip",  {  templateUrl : "html/products/cip.html"  })
     .when("/products/mes",  {  templateUrl : "html/products/mes.html"  })
+    .when("/products/ips",  {  templateUrl : "html/products/ips.html"  })
+
     .when("/aboutus",     {  templateUrl : "html/aboutus.html"  })
     .when("/company",   {  templateUrl : "html/company.html"   })
-
     .when("/solutions", {  templateUrl : "html/solutions.html" })
 
     .otherwise({redirectTo:'/home'});
